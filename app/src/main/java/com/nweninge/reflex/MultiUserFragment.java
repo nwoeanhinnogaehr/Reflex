c218bcaf93dec669c45817fcbcf044eef69aeee0
Noah Weninger <nweninge@ualberta.ca>
2015-09-27 14:19:08 -0600

    Add basic multi user layout

--- app/src/main/java/com/nweninge/reflex/MultiUserFragment.java
+++ app/src/main/java/com/nweninge/reflex/MultiUserFragment.java
@@ -43,6 +43,7 @@ public class MultiUserFragment extends Fragment {
         if (getArguments() != null) {
             recordDb = (RecordDatabase)getArguments().getSerializable(ARG_DB);
         }
+        getActivity().findViewById(R.id.button1);
     }
 
     @Override
